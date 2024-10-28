82ae67e8efc83d8d98afd6b6a4a8c332edfae8d9
djLee0831 <dreamsoft00@knou.ac.kr>
2024-10-28 12:04:57 +0900

    logging 추가

--- config/production.py
+++ config/production.py
@@ -1,5 +1,30 @@
+from logging.config import dictConfig
+
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x11A\x9fpZ\x9e\x1fZ\x88\x82y\x02\xe6\xf2\x97\xfa'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
